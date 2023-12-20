77dcefacb109e8cf0e0ea0ffb00ff05aa6e872f4
angela-001-g <105566778+angela-001-g@users.noreply.github.com>
2023-12-20 15:16:04 -0500

    finalizando estilos

--- ReactCh/src/components/Timer.jsx
+++ ReactCh/src/components/Timer.jsx
@@ -3,7 +3,17 @@ import "../styles/timer.css";
 function Timer() {
   return (
     <>
-      <div className="divTimer">Hola</div>
+      <div className="divTimer">
+        <div className="divLetters">
+          <h1>Timer</h1>
+          <b>0 mins 0 secs</b>
+        </div>
+        <div className="divButtons">
+          <button className="butStart">Start</button>
+          <button className="butStop">Stop</button>
+          <button className="butReset">Reset</button>
+        </div>
+      </div>
     </>
   );
 }
